8ff39b64dfb80b514b0e37a4bb226340ca4396c7
Fumycat <fumycat@gmail.com>
2017-03-29 11:34:41 +0700

    Code cleanup Auto skipping already downloaded files Progressbar removed

--- manga-dl.py
+++ manga-dl.py
@@ -1,33 +1,18 @@
 #!/usr/bin/env python3
-import os
-
 import click
-import requests
 
 
 @click.command()
 @click.argument('url', type=str)
 @click.option('--skip', default=0, type=int, help='Skip first n chapters')
-@click.option('--without-progressbar', is_flag=True)
-def main(url, skip, without_progressbar):
+def main(url, skip):
     if 'mangaclub.ru' in url:
-        from mangaclubru import get_view
-        from mangaclubru import parse
-        from mangaclubru import FILE_EXTENSION
+        from mangaclubru import process
     else:
         exit('Wrong url')
 
-    main_page_url = get_view(requests.get(url).text)
-    for o in parse(requests.get(main_page_url).text, skip, without_progressbar):
-        if not os.path.exists(o.get('name') + '/' + o.get('chapter')):
-            os.makedirs(o.get('name') + '/' + o.get('chapter'))
-
-        file_path = o.get('name') + '/' + o.get('chapter') + '/' + o.get('page') + FILE_EXTENSION
-        response = requests.get(o.get('image'))
-        with open(file_path, 'wb') as out:
-            out.write(response.content)
-        del response
-    exit('Completed')
+    process(url, skip)
+    exit('Done')
 
 
 if __name__ == '__main__':
--- mangaclubru.py
+++ mangaclubru.py
@@ -1,27 +1,26 @@
+import os
+
 import requests as http
+from requests.adapters import HTTPAdapter
 from bs4 import BeautifulSoup
 
+ROOT_URL = 'https://mangaclub.ru/'
 VIEW_URL = 'https://mangaclub.ru/manga/view/'
 FILE_EXTENSION = '.jpg'
 
 
-def get_view(page):
-    soup = BeautifulSoup(page, 'html.parser')
-    div = soup.find('div', {'class': 'read_manga'})
+def process(url, skip):
+    """
+    Returns image link generator
+    """
+    main_page = BeautifulSoup(http.get(url).text, 'html.parser')
+    div = main_page.find('div', {'class': 'read_manga'})
     if div.a.get('href') == 'javascript:document.write(AdultManga)':
         view_id = div.script.string.split('/view/')[1].split('"')[0]
     else:
         view_id = div.a.get('href').split('/view/')[1]
-    return VIEW_URL + view_id
 
-
-def parse(page, skip, without_progressbar):
-    """
-    Returns image link generator
-    """
-    if not without_progressbar:
-        import progressbar
-    soup = BeautifulSoup(page, 'html.parser')
+    soup = BeautifulSoup(http.get(VIEW_URL + view_id).text, 'html.parser')
     name = soup.find('div', {'class': 'head-body container'}).h2.a.string.replace('/', '-')
     print('Getting chapter list for ' + name)
     chapter_list = soup.find('div', {'class': 'manga-thumbs-chapters scroll-chapters'})
@@ -29,23 +28,27 @@ def parse(page, skip, without_progressbar):
     for link in chapter_list.find_all('a'):
         chapters.append(link.get('href'))
     c = len(chapters)
-    print('Found {} chapters\nDownloading...'.format(str(c)))
+    print('Found', c, 'chapters')
     if skip > 0:
         print('Skipping first {} chapters'.format(str(skip)))
-    chapters = chapters[skip:]
+        chapters = chapters[skip:]
+
+    session = http.Session()
+    session.mount(ROOT_URL, HTTPAdapter(max_retries=20))
     # main loop
-    n = 1 + skip
-    if not without_progressbar:
-        bar = progressbar.ProgressBar(max_value=c, widgets=[progressbar.SimpleProgress(), progressbar.Bar()])
     for a in chapters:
-        if without_progressbar:
-            print(n, 'of', c)
-        else:
-            bar.update(n)
-        soup = BeautifulSoup(http.get(a).text, 'html.parser')
-        for manga_link in soup.find('div', {'class': 'manga-lines-pages'}).find_all('a'):
-            yield {'name': name,
-                   'chapter': manga_link.get('data-c'),
-                   'page': manga_link.get('data-p'),
-                   'image': manga_link.get('data-i')}
-        n += 1
+        soup = BeautifulSoup(session.get(a).text, 'html.parser')
+        current_chapter_pages = soup.find('div', {'class': 'manga-lines-pages'}).find_all('a')
+        chapter_id = current_chapter_pages[0].get('data-c')
+        print('Processing chapter', chapter_id)
+        current_chapter_path = name + '/' + chapter_id
+        if not os.path.exists(current_chapter_path):
+            os.makedirs(current_chapter_path)
+        for page in current_chapter_pages:
+            file_path = current_chapter_path + '/' + page.get('data-p') + FILE_EXTENSION
+            if os.path.exists(file_path):
+                continue
+            response = session.get(page.get('data-i'))
+            with open(file_path, 'wb') as out:
+                out.write(response.content)
+            del response
